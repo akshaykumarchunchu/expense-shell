1a41877fb8887f96829704ddb9714e37afa38467
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-23 18:35:58 -0400

    some message

--- mysql.sh
+++ mysql.sh
@@ -6,6 +6,11 @@ TIMESTAMP=$(date +%f-H%-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
