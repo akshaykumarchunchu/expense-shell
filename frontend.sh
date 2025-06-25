0b19032d76695a99781827ccffc187cd18981941
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-24 20:40:15 -0400

    expenses project

---
+++ frontend.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2..$R is failure"
+     else
+        echo "$2..$G is Success"
+    fi
+}
+
+if [ $userid -u ]
+then 
+    echo "you're not a superuser"
+    exit 1
+else
+    echo "youre a superuser"
+fi
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "enable nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "start nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "removing default content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "downloading the file"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+VALIDATE $? "unzip the frontend document"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "restart nginx"
+
