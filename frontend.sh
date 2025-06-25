2346d0f0de860c1a27e90d5169ba3e85656fc31b
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-24 20:49:28 -0400

    expenses project

--- frontend.sh
+++ frontend.sh
@@ -47,6 +47,9 @@ cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "unzip the frontend document"
 
+cp /home/ec2-user/expense-shell/backend.akshaydaws-78s.online /etc/nginx/default.d/expense.conf
+VALIDATE $? "copied backend expense conf"
+
 systemctl restart nginx &>>$LOGFILE
 VALIDATE $? "restart nginx"
 
